918677508931f2bb13ebe00ba14e63427ae9abed
Ian Sowinski <sowinj@gmail.com>
2016-06-25 21:21:51 +0200

    zmiany w js

--- photography/animacja.js
+++ photography/animacja.js
@@ -26,3 +26,57 @@ $(document).ready(function(){
     }
   });
 });
+$(document).keydown(function(e){
+  switch (e.which) {
+    case 37: //left
+    if (y>0) {
+      $('#'+y).fadeOut(1);
+      y = y - 1;
+      $('#'+y).fadeIn(1);
+    }
+    else {
+      $('#'+y).fadeOut(1);
+      y = x;
+      $('#'+y).fadeIn(1);
+    }
+      break;
+    case 39: //right
+    if (y<x) {
+      $('#'+y).fadeOut(1);
+      y = y + 1;
+      $('#'+y).fadeIn(1);
+    }
+    else {
+      $('#'+y).fadeOut(1);
+      y = 0;
+      $('#'+y).fadeIn(1);
+    }
+      break;
+    default: return
+
+  }
+  $('.next').click(function(){
+    if (y<x) {
+      $('#'+y).fadeOut(1);
+      y = y + 1;
+      $('#'+y).fadeIn(1);
+    }
+    else {
+      $('#'+y).fadeOut(1);
+      y = 0;
+      $('#'+y).fadeIn(1);
+    }
+  });
+  $('.prev').click(function(){
+    if (y>0) {
+      $('#'+y).fadeOut(1);
+      y = y - 1;
+      $('#'+y).fadeIn(1);
+    }
+    else {
+      $('#'+y).fadeOut(1);
+      y = x;
+      $('#'+y).fadeIn(1);
+    }
+  });
+});

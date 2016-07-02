0197b03fbba29236b27e11575139e9de5db4687e
Ian Sowinski <sowinj@gmail.com>
2016-07-02 14:47:54 +0200

    zmieniony motyw i zmieniona glaeria gromu

--- photography/animacja.js
+++ photography/animacja.js
@@ -1,6 +1,6 @@
-x = x - 1;
-var y = 0;
-$(document).ready(function(){
+x = x - 1; // x  - number of slides in slideshow
+var y = 0; // y - counter for slideshow
+$(document).ready(function(){ // Function for 'next' and 'prev' navigation
   $('.next').click(function(){
     if (y<x) {
       $('#'+y).fadeOut(1);
@@ -26,7 +26,7 @@ $(document).ready(function(){
     }
   });
 });
-$(document).keydown(function(e){
+$(document).keydown(function(e){ // function for keyboard arrows navigation
   switch (e.which) {
     case 37: //left
     if (y>0) {
@@ -53,30 +53,5 @@ $(document).keydown(function(e){
     }
       break;
     default: return
-
   }
-  $('.next').click(function(){
-    if (y<x) {
-      $('#'+y).fadeOut(1);
-      y = y + 1;
-      $('#'+y).fadeIn(1);
-    }
-    else {
-      $('#'+y).fadeOut(1);
-      y = 0;
-      $('#'+y).fadeIn(1);
-    }
-  });
-  $('.prev').click(function(){
-    if (y>0) {
-      $('#'+y).fadeOut(1);
-      y = y - 1;
-      $('#'+y).fadeIn(1);
-    }
-    else {
-      $('#'+y).fadeOut(1);
-      y = x;
-      $('#'+y).fadeIn(1);
-    }
-  });
 });

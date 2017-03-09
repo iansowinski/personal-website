35abd8284ee7dacecbbc9772ed2da57a9e1434bb
Ian Sowinski <sowinj@gmail.com>
2017-03-09 01:03:19 +0100

    improved animation

--- animacja.js
+++ animacja.js
@@ -3,26 +3,26 @@ var y = 0; // y - counter for slideshow
 $(document).ready(function(){ // Function for 'next' and 'prev' navigation
   $('.next').click(function(){
     if (y<x) {
-      $('#'+y).fadeOut(1);
-      y = y + 1;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = y + 1;
+        $('#'+y).fadeIn("slow");});
     }
     else {
-      $('#'+y).fadeOut(1);
-      y = 0;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = 0;
+        $('#'+y).fadeIn("slow");});
     }
   });
   $('.prev').click(function(){
     if (y>0) {
-      $('#'+y).fadeOut(1);
-      y = y - 1;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = y - 1;
+        $('#'+y).fadeIn("slow");});
     }
     else {
-      $('#'+y).fadeOut(1);
-      y = x;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = x;
+        $('#'+y).fadeIn("slow");});
     }
   });
 });
@@ -30,26 +30,30 @@ $(document).keydown(function(e){ // function for keyboard arrows navigation
   switch (e.which) {
     case 37: //left
     if (y>0) {
-      $('#'+y).fadeOut(1);
-      y = y - 1;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = y - 1;
+        $('#'+y).fadeIn("slow");
+      });
     }
     else {
-      $('#'+y).fadeOut(1);
-      y = x;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = x;
+        $('#'+y).fadeIn("slow");
+      });
     }
       break;
     case 39: //right
     if (y<x) {
-      $('#'+y).fadeOut(1);
-      y = y + 1;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = y + 1;
+        $('#'+y).fadeIn("slow");
+      });
     }
     else {
-      $('#'+y).fadeOut(1);
-      y = 0;
-      $('#'+y).fadeIn(1);
+      $('#'+y).fadeOut("slow", function(){
+        y = 0;
+        $('#'+y).fadeIn("slow");
+      });
     }
       break;
     default: return

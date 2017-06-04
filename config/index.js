73f3fa5428375b905dbae80c4cf0a92e478d5d9f
Ian Sowinski <sowinj@gmail.com>
2017-06-04 17:41:19 +0200

    added vps

--- config/index.js
+++ config/index.js
@@ -21,6 +21,25 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   },
+  vps: {
+    env: require('./prod.env'),
+    index: '/var/www/html/index.html',
+    assetsRoot: '/var/www/html/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
   dev: {
     env: require('./dev.env'),
     port: 8080,
